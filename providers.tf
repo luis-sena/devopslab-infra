23638a18704093d8a06fbb1de990d51402e9fb72
Luis Sena <luisfj.sena@gmail.com>
2021-10-23 12:19:18 -0300

    first commit

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.89.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("./gcp-key.json")
+
+  project = "lab-devops-cloud-lsena"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
